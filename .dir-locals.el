5d0c15ceea8cf03ef05e2a60b7dd0fa7d9237da3
David A. Ventimiglia <dventimi@gmail.com>
2014-11-08 09:52:10 -0800

    Made the compile command a Maven command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((nil
-  (compile-command . (concat "mvn -B compile " (let ((l (dir-locals-find-file (or (buffer-file-name) default-directory)))) (if (listp l) (car l) l))))))
+((java-mode
+  (compile-command . (concat "cd " (let ((l (dir-locals-find-file (or (buffer-file-name) default-directory)))) (if (listp l) (car l) l)) " && mvn -B compile"))))
