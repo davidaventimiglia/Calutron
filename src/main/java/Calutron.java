a0ec18741d25ed148ed459a0ecaa1bbc271b5848
David A. Ventimiglia <dventimi@gmail.com>
2014-11-08 09:52:26 -0800

    Got a rudimentary command processor to work

--- src/main/java/Calutron.java
+++ src/main/java/Calutron.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import javax.xml.bind.*;
 import org.apache.olingo.odata2.api.commons.*;
 import org.apache.olingo.odata2.api.edm.*;
@@ -31,32 +32,110 @@ public class Calutron {
 
     // Commands
 
-    private static void setServiceUrl (Console console) {SERVICE_URL = console.readLine("Service URL: ");}
-    private static void setUserName (Console console) {USERNAME = console.readLine("Username: ");}
-    private static void setPassword (Console console) {PASSWORD = console.readLine("Password: ");}
-    private static void showMetaData (Console console) throws IOException, ODataException {System.out.println(readEdm(SERVICE_URL));}
-    private static void setContentTypeXML () {CONTENT_TYPE = APPLICATION_XML;}
-    private static void setContentTypeJSON () {CONTENT_TYPE = APPLICATION_JSON;}
-    private static void showServiceDoc (Console console) throws IOException, ODataException {}
-    private static void quit () {System.exit(0);}
+    private static interface Command {
+        String getCommandString ();
+        void setCommandString (String command);
+        void execute ();}
+
+    private static class NullArgumentException extends IllegalArgumentException {
+        public NullArgumentException (Object param) {
+            super(String.format("The `%s' parameter cannot be null.", param));}
+        public NullArgumentException (Object param, Throwable cause) {
+            super(String.format("The `%s' parameter cannot be null.", param), cause);}
+        public NullArgumentException (Throwable cause) {
+            super(cause);}}
+
+    private static class StoppedException extends RuntimeException {
+        public StoppedException (Throwable t) {
+            super(t);}}
+
+    private static abstract class AbstractCommand implements Command {
+        String commandString = null;
+        Calutron calutron = null;
+        public AbstractCommand (final String command) {
+            if (command==null) throw new NullArgumentException("command");
+            setCommandString(command);}
+        public AbstractCommand (final Calutron calutron, final String command) {
+            if (calutron==null) throw new NullArgumentException("calutron");
+            if (command==null) throw new NullArgumentException("command");
+            this.calutron = calutron;
+            setCommandString(command);}
+        public void setCalutron (Calutron calutron) {
+            if (calutron==null) throw new NullArgumentException("calutron");
+            this.calutron = calutron;}
+        public Calutron getCalutron () {
+            return calutron;}
+        public String getCommandString () {
+            return commandString.toUpperCase();}
+        public void setCommandString (String command) {
+            if (command==null) throw new NullArgumentException("command");
+            commandString = command.replaceAll("\\s+", " ").toUpperCase().trim();}}
+
+    private static class CommandMap extends TreeMap<String, Command> {
+        public void add (final Command command) {
+            if (command==null) throw new NullArgumentException("command");
+            super.put(command.getCommandString(), command);}
+        public void add (final Command... commands) {
+            if (commands==null) throw new NullArgumentException("commands");
+            for (int i=0; i<commands.length; i++) add(commands[i]);}}
+
+    protected final CommandMap commands = new CommandMap();
+    protected Console console = null;
+    protected final Properties settings = new Properties();
+
+    public CommandMap getCommands () {return commands;}
+
+    public Properties getSettings () {return settings;}
+
+    public String getSetting (final String name) {
+        return getSettings().getProperty(name);}
+
+    public void setSetting (final String name, final String value) {
+        if (name==null) throw new NullArgumentException("name");
+        if (value==null) throw new NullArgumentException("value");
+        getSettings().setProperty(name, value);}
+
+    public Console getConsole () {return console;}
+
+    public Calutron (final Console console) {
+        this.console = console;}
+
+    public Calutron (final Console console, final Properties settings) {
+        this(console);
+        this.settings.putAll(settings);}
+
+    public void executeCommand (final String name) {
+        if (name==null) throw new NullArgumentException("name");
+        getCommand(name).execute();}
+    
+    public void addCommands (final Command... commands) {
+        this.commands.add(commands);}
+
+    public Command getCommand (final String name) {
+        if (name==null) throw new NullArgumentException("name");
+        if (getCommands().containsKey(name.toUpperCase())) return getCommands().get(name.toUpperCase());
+        return new AbstractCommand(this, "default") {public void execute () {getConsole().printf("%s\n", "Command not found");}};}
+
+    public void start () {
+        while (true) try {getCommand(getConsole().readLine("CALUTRON>").replaceAll("\\s+", " ").trim()).execute();} catch (NullPointerException e) {throw new StoppedException(e);}}
 
     // Main Loop
 
     public static void main (String[] args) throws IOException, ODataException {
         Console console = System.console();
         if (console==null) System.exit(1);
-        setContentTypeXML();
-        while (true) {
-            String input = console.readLine("CALUTRON>").replaceAll("\\s+", " ").trim();
-            if (input.equalsIgnoreCase("set service url")) setServiceUrl(console);
-            if (input.equalsIgnoreCase("show metadata")) showMetaData(console);
-            if (input.equalsIgnoreCase("set username")) setUserName(console);
-            if (input.equalsIgnoreCase("set password")) setPassword(console);
-            if (input.equalsIgnoreCase("set out xml")) setContentTypeXML();
-            if (input.equalsIgnoreCase("set out json")) setContentTypeJSON();
-            if (input.equalsIgnoreCase("show service doc")) showServiceDoc(console);
-            if (input.equalsIgnoreCase("quit")) quit();
-            if (input.equalsIgnoreCase("exit")) quit();}}
+        Calutron calutron = new Calutron(console);
+        calutron.addCommands(new AbstractCommand(calutron, "quit") {public void execute () {System.exit(0);}},
+                             new AbstractCommand(calutron, "help") {public void execute () {for (String s : getCalutron().getCommands().keySet()) getCalutron().getConsole().printf("%s\n", s);}},
+                             new AbstractCommand(calutron, "show commands") {public void execute () {for (String s : getCalutron().getCommands().keySet()) getCalutron().getConsole().printf("%s\n", s);}},
+                             new AbstractCommand(calutron, "set password") {public void execute () {getCalutron().setSetting("PASSWORD", new String(getCalutron().getConsole().readPassword("%s", "Password:")));}},
+                             new AbstractCommand(calutron, "set service url") {public void execute () {getCalutron().setSetting("SERVICE_URL", getCalutron().getConsole().readLine("Service URL: "));}},
+                             new AbstractCommand(calutron, "show service doc") {public void execute () {}},
+                             new AbstractCommand(calutron, "show metadata") {public void execute () {try {getCalutron().getConsole().printf("%s\n", readEdm(getCalutron().getSettings().getProperty("SERVICE_URL")));} catch (Throwable t) {throw new RuntimeException(t);}}},
+                             new AbstractCommand(calutron, "set username") {public void execute () {getCalutron().setSetting("USERNAME", getCalutron().getConsole().readLine("Username: "));}});
+        try {calutron.start();}
+        catch (StoppedException e) {System.exit(0);}
+        catch (Throwable t) {System.exit(1);}}
 
     // Helper methods
 
